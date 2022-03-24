bc64de330a639b3e94d620bf43a425cdf089e254
Hong Minhee <hong.minhee@planetariumhq.com>
2022-03-24 21:17:07 +0900

    Explanation for curiositarians
    [ci skip]

---
+++ sur/__init__.py
@@ -0,0 +1,8 @@
+""sur --- Submodule Updater
+
+Anyway, what does sur stand for?  It's nothing as of now.  Actually I named it
+after submodule-update-request, but as its architecture and goals were slighly
+shifted, I had to rename it to Submodule Updater.  However, the Python package
+name sur still remains as it has been, because it's quite bother to rename.
+
+"""
